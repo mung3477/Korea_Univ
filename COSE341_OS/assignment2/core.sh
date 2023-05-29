9b6becf4604a8affef0b384a46e1e6e89343c3cd
mung3477 <mung3477@naver.com>
2023-05-29 20:58:25 +0900

    feat(COSE341): finished experiment

--- COSE341_OS/assignment2/core.sh
+++ COSE341_OS/assignment2/core.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 ITER=5
-EXEC_TIME=60
+EXEC_TIME=300
 
 cd /sys/fs/cgroup/cpuset
 mkdir -p mycpu; cd mycpu
@@ -9,7 +9,7 @@ echo 0 > cpuset.mems
 echo $$ > tasks
 cat tasks
 
-dmesg -C
+sudo dmesg -C
 
 cd ~/Desktop/KU/COSE341_OS/assignment2
 touch 1ms_result.txt
@@ -20,7 +20,7 @@ gcc cpu.c
 # 1ms timeslice
 sudo echo 1 > /proc/sys/kernel/sched_rr_timeslice_ms
 sudo ./a.out 2 $EXEC_TIME | sed -n '/total/p' > 1ms_output.txt
-for (( c=1; c<=$ITER; c++))
+for (( c=2; c<=$ITER; c++))
 do
 	sudo ./a.out 2 $EXEC_TIME | sed -n '/total/p' >> 1ms_output.txt
 done
@@ -30,16 +30,16 @@ grep "#0  totalCount" 1ms_output.txt | awk -v ITER=$ITER '{sum_1 += $5} END { pr
 grep "#1  totalCount" 1ms_output.txt | awk -v ITER=$ITER '{sum_2 += $5} END { printf "PROCESS #1 average calculation on 1ms: %s\n", sum_2 /= ITER >> "1ms_result.txt" }'
 
 awk '{total += $7} END { printf "Total calculation: %s\n", total >> "1ms_result.txt"}' 1ms_result.txt
-dmesg -s 32000000 | grep "CPUburst" | awk '{sum += $5} END { printf "Total CPUburst: %s\n", sum >> "1ms_result.txt"}'
+sudo dmesg -s 32000000 | grep "CPUburst" | awk '{sum += $5} END { printf "Total CPUburst: %s\n", sum >> "1ms_result.txt"}'
 cat 1ms_result.txt
 grep "Total" 1ms_result.txt | awk '{ arr[NR] = $3 } END { print arr[1] / arr[2] }'
 
-dmesg -C
+sudo dmesg -C
 
 # 10ms timeslice
 sudo echo 10 > /proc/sys/kernel/sched_rr_timeslice_ms
 sudo ./a.out 2 $EXEC_TIME | sed -n '/total/p' > 10ms_output.txt
-for (( c=1; c<=$ITER; c++))
+for (( c=2; c<=$ITER; c++))
 do
 	sudo ./a.out 2 $EXEC_TIME | sed -n '/total/p' >> 10ms_output.txt
 done
@@ -49,18 +49,18 @@ grep "#0  totalCount" 10ms_output.txt | awk -v ITER=$ITER '{sum_1 += $5} END { p
 grep "#1  totalCount" 10ms_output.txt | awk -v ITER=$ITER '{sum_2 += $5} END { printf "PROCESS #1 average calculation on 10ms: %s\n", sum_2 /= ITER >> "10ms_result.txt" }'
 
 awk '{total += $7} END { printf "Total calculation: %s\n", total >> "10ms_result.txt"}' 10ms_result.txt
-dmesg -s 32000000 | grep "CPUburst" | awk '{sum += $5} END { printf "Total CPUburst: %s\n", sum >> "10ms_result.txt"}'
+sudo dmesg -s 32000000 | grep "CPUburst" | awk '{sum += $5} END { printf "Total CPUburst: %s\n", sum >> "10ms_result.txt"}'
 cat 10ms_result.txt
 grep "Total" 10ms_result.txt | awk '{ arr[NR] = $3 } END { print arr[1] / arr[2] }'
 
-dmesg -C
+sudo dmesg -C
 
 # 100ms timeslice
 sudo echo 100 > /proc/sys/kernel/sched_rr_timeslice_ms
 sudo ./a.out 2 $EXEC_TIME | sed -n '/total/p' > 100ms_output.txt
-for (( c=1; c<=$ITER; c++))
+for (( c=2; c<=$ITER; c++))
 do
-	sudo ./a.out 2 $EXEC_TIME > | sed -n '/total/p' >> 100ms_output.txt
+	sudo ./a.out 2 $EXEC_TIME | sed -n '/total/p' >> 100ms_output.txt
 done
 
 # 100ms output data analysis
@@ -68,7 +68,7 @@ grep "#0  totalCount" 100ms_output.txt | awk -v ITER=$ITER '{sum_1 += $5} END { 
 grep "#1  totalCount" 100ms_output.txt | awk -v ITER=$ITER '{sum_2 += $5} END { printf "PROCESS #1 average calculation on 100ms: %s\n", sum_2 /= ITER >> "100ms_result.txt" }'
 
 awk '{total += $7} END { printf "Total calculation: %s\n", total >> "100ms_result.txt"}' 100ms_result.txt
-dmesg -s 32000000 | grep "CPUburst" | awk '{sum += $5} END { printf "Total CPUburst: %s\n", sum >> "100ms_result.txt"}'
+sudo dmesg -s 32000000 | grep "CPUburst" | awk '{sum += $5} END { printf "Total CPUburst: %s\n", sum >> "100ms_result.txt"}'
 cat 100ms_result.txt
 grep "Total" 100ms_result.txt | awk '{ arr[NR] = $3 } END { print arr[1] / arr[2] }'
 
